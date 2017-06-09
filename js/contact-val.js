ac6f2fab22ddc183760afb782be45c90d91fbf36
Joz <joz@showcaseimagery.com>
2017-06-09 12:55:20 +0100

    fixed contact form error
    -- success dialog box error

--- js/contact-val.js
+++ js/contact-val.js
@@ -76,7 +76,7 @@ if("undefined"==typeof jQuery)throw new Error("BootstrapValidator requires jQuer
             }
         })
         .on('success.form.bv', function(e) {
-            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+            $('#success_message').slideDown('fast').delay(1000).fadeOut('slow').delay(7000)
                 $('#contact_form').data('bootstrapValidator').resetForm();
 
             // Prevent form submission
